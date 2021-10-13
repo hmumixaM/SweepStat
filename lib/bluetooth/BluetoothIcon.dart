50aa70ee014d24dcc7da9cd4bdd1a73f097d5d27
Matt Wasyluk <mwasyluk@unc.edu>
2021-10-13 15:21:24 -0400

    progress on bluetooth drawer, needs linking and testing

--- lib/bluetooth/BluetoothIcon.dart
+++ lib/bluetooth/BluetoothIcon.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class BluetoothIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: () {}, icon: Icon(Icons.bluetooth_disabled));
+    return IconButton(onPressed: () {
+      //link to bluetooth device page in next iteration
+    },
+        icon: Icon(Icons.bluetooth_disabled));
   }
 }
