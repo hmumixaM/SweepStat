1cc5c4c5003b155fdd7801827e7d151ce42c2906
Matt Wasyluk <mwasyluk@unc.edu>
2021-11-08 22:48:06 -0500

    config save button now commits to db

--- test/file_management/file_manager_test.dart
+++ test/file_management/file_manager_test.dart
@@ -3,6 +3,7 @@ import 'package:sweep_stat_app/file_management/file_manager.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sweep_stat_app/experiment/ExperimentSettings.dart';
 
 void main() async{
 
@@ -14,23 +15,42 @@ void main() async{
     expect(initQuery.length == 1, true);
   });
 
-  var input = ConfigSave(
-    title: "Valid Test Input",
-    initialV: .5,
-    vertexV: 2.5,
-    finalV: .5,
+  var validCVInput = VoltammetrySettings(
+    initialVoltage: .5,
+    vertexVoltage: 2.5,
+    finalVoltage: .5,
     scanRate: .2,
     sampleInterval: .01,
-    segmentCount: 6,
-  );
-  await DBManager.addObject(db, EntryType.config, input.toMap());
-  List addQuery = await db.query('configs', where: 'title=?', whereArgs: ["Valid Test Input"]);
-  test("valid input is successfully stored", () {
-    expect(addQuery.length == 1, true);
+    sweepSegments: 6,
+    gainSetting: GainSettings.uA1,
+    electrode: Electrode.silver,
+  ).toDBMap("Valid CV Input");
+  await DBManager.addObject(db, EntryType.config, validCVInput);
+  Map addQuery1 = await DBManager.queryExpectOneResult(db, EntryType.config, validCVInput["title"]);
+  test("valid CV config is successfully stored", () {
+    expect(addQuery1["title"] == validCVInput["title"], true);
   });
 
-  await DBManager.deleteObject(db, EntryType.config, input.title);
-  List deleteQuery = await db.query('configs', where: 'title=?', whereArgs: ["Valid Test Input"]);
+  Map validAmperometryInput = AmperometrySettings(
+    initialVoltage: .5,
+    sampleInterval: .01,
+    runtime: 3.6,
+    gainSetting: GainSettings.uA1,
+    electrode: Electrode.silver,
+  ).toDBMap("Valid Amperometry Input");
+  await DBManager.addObject(db, EntryType.config, validAmperometryInput);
+  Map addQuery2 = await DBManager.queryExpectOneResult(db, EntryType.config, validAmperometryInput["title"]);
+  test("valid amperometry config is successfully stored", () {
+    expect(addQuery2["title"] == validAmperometryInput["title"], true);
+  });
+
+  List tableTestQuery = await DBManager.queryEntireTable(db, EntryType.config);
+  test("table query returns successfully and accurately", () {
+    expect(tableTestQuery.length == 2, true);
+  });
+
+  await DBManager.deleteObject(db, EntryType.config, validCVInput["title"]);
+  List deleteQuery = await db.query('configs', where: 'title=?', whereArgs: [validCVInput["title"]]);
   test("existing input is successfully deleted/nonexistent object not found", () {
     expect(deleteQuery.length == 0, true);
   });
